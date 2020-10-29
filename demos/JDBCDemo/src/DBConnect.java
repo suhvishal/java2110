2242b633d7d42f7097530c5e9bc5060a4f07ac95
vishal <23210008@mkcl.org>
2020-10-29 10:11:13 +0530

    -

--- demos/JDBCDemo/src/DBConnect.java
+++ demos/JDBCDemo/src/DBConnect.java
@@ -1,25 +1,28 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnect {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		try {
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "suhroot123")) {
 			//load db drivers  
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "suhroot123");
-			
 			//create statement object 
+			Statement stmt = con.createStatement();
 			
+			//execute the sql statement 
+			String sql = "INSERT INTO employee (empId, empName, city, salary) VALUES (101, 'Mitul', 'Mumbai', 2000.00)";
 			
-			//execute the statement 
+			int result = stmt.executeUpdate(sql);
 			
-			
-			//close the connection
+			if(result>0) {
+				System.out.println("Record is inserted...");
+			}
 			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block

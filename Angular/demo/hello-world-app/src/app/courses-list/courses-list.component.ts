7ecb10a553b79169a351050558514d0ac2eaec85
vishal <23210008@mkcl.org>
2020-11-02 17:13:11 +0530

    -

--- Angular/demo/hello-world-app/src/app/courses-list/courses-list.component.ts
+++ Angular/demo/hello-world-app/src/app/courses-list/courses-list.component.ts
@@ -1,4 +1,6 @@
+import { core } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course.model';
 
 @Component({
   selector: 'app-courses-list',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesListComponent implements OnInit {
 
-  constructor() { }
+  public ourCourses: Course[];
+
+  constructor() {
+
+      this.ourCourses = [
+         new Course(1,  'Angular', '5 days', 'Vishal'),
+         new Course(2,  'React', '5 days', 'Vishal'),
+         new Course(3,  'Java', '7 days', 'Vishal'),
+         new Course(4,  'Spring', '2 days', 'Vishal'),
+         new Course(5,  'Hibernate', '6 days', 'Vishal'),
+        ]
+
+   }
 
   ngOnInit(): void {
   }
---
+++ Angular/demo/hello-world-app/src/app/model/course.model.ts
@@ -0,0 +1,11 @@
+export class Course {
+    
+
+    constructor(public courseId: number,
+                public courseName: string,
+                public duration: string,
+                public trainer: string){
+
+        
+    }
+}

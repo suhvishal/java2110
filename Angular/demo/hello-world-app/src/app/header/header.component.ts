c6ffebe1cd8c630aa9967861871a58aaee9e805e
vishal <23210008@mkcl.org>
2020-11-04 09:01:28 +0530

    -

--- Angular/demo/hello-world-app/src/app/header/header.component.ts
+++ Angular/demo/hello-world-app/src/app/header/header.component.ts
@@ -9,11 +9,20 @@ export class HeaderComponent implements OnInit {
 
   public companyName:string = "Zycus"; 
   public imageUrl:string = "/assets/zycus-logo1.png";
+  public isSpecial: boolean = true;
+
+  public someStyles = {
+    'font-style' : this.isSpecial ? 'italic' : 'normal',
+    'font-weight' : this.isSpecial ? 'bold' : 'normal',
+    'font-size' : this.isSpecial ? '24px' : '16px',
+    'color' : this.isSpecial ? 'green' : 'black'
+  }
 
   constructor() { 
     setTimeout(() => {
       //this.companyName = "Zycus Mumbai"
-      this.imageUrl = "/assets/zycus-logo2.png"
+      //this.imageUrl = "/assets/zycus-logo2.png"
+      this.isSpecial = false;
     }, 7000);
   }
 
